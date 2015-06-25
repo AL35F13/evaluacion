a936e83c5df32c9f42708f7e6d255bf95d7bd85e
AL35F13 <anahi2028@gmail.com>
2015-06-25 11:01:57 -0500

    uno
    aplicacion de musica para evaluacion

---
+++ js/acciones.js
@@ -0,0 +1,153 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	
+	$('#btn_1D').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_1D01').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_1D02').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_1D03').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_1D04').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_austin').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_austin01').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_austin03').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_austin04').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_austin05').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_cantantes').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_cantantes01').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_cantantes02').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_cantantes03').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_cantantes04').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_cantantes05').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_haash').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_haash02').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_haash03').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_haash04').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_haash05').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_introduccion').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_introduccion01').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_introduccion02').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_introduccion03').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_introduccion04').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_katy').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_katy01').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_katy02').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_katy04').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_katy05').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_martin').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_martin01').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_martin02').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_martin03').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+	
+	$('#btn_martin05').on ('tap', function() {
+	navigator.vibrate(1000);
+	});
+ 
+
+	
+}); 
+});
+
